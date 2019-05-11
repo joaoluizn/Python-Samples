6689ffec0f24bdced9d390f4b72cdc1201bf2b5d
John Luiz <joao.zenos@gmail.com>
2019-05-11 19:53:58 -0300

    Positional and Key Arguments Sample

---
+++ arguments.py
@@ -0,0 +1,25 @@
+""" General Arguments Types Samples """
+
+def hello_arguments(required_one, *args, **kwargs):
+    return f"Required Arg: {required_one}\nPosititonal Args: {args}\nKey Args: {kwargs}\n"
+
+""" 
+Values are just samples to represent arguments. 
+Focus on how apply different types of args.
+Note: 
+    Positional args are mapped to a tuple;
+    Key Arguments are mapped to dictionary with key and values;
+"""
+
+if __name__ == "__main__":
+    # Required argument, note positional and key args aren't obligatory
+    print(hello_arguments(10))
+
+    # Required and 2 positional args
+    print(hello_arguments(10, 20, 30))
+
+    # Required, positional and key arguments, possibilities starts to grow.
+    print(hello_arguments(10, 20, 30, 40, id=20, value=30))
+
+    # Required and Key Arguments.
+    print(hello_arguments(10, id=20, value=30))
